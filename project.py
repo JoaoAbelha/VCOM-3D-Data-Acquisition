051b27b105a61f0bcd0297a3fe8e1bdada0d33f5
vitorb19 <vitorhugobarbosa13@gmail.com>
2021-04-12 03:02:41 +0100

    Merge steps

--- project.py
+++ project.py
@@ -5,18 +5,21 @@ from matplotlib import pyplot as plt
 import math
 from step1 import camera_calibration
 from step2 import camera_position
-from step5 import getPoints
+from step5_2 import getShadowPoints
 from step6 import shadow3DPoints
 
 CAMERA_POSITION_IMG = 'calibration/GOPR0032.jpg'
+IMG = '/imgs/alternate/Untitled_000037.png'
 
 
 def main():
     print("Hello World!")
     camera_calibration()
-    image = cv2.imread(CAMERA_POSITION_IMG)
-    position_normalized, projection_matrix = camera_position(image)
-    shadow_plane = []
+    chessBoardImage = cv2.imread(CAMERA_POSITION_IMG)
+    position_normalized, projection_matrix = camera_position(chessBoardImage)
+    image = cv2.imread(IMG)
+    shadowPoints = getShadowPoints(image)
+    print(shadow3DPoints(shadowPoints, projection_matrix))
 
 
 if __name__ == "__main__":
