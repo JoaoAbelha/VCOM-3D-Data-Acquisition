60ab4269d62211ba23c38764bd6aed0f2bf8a8d5
Gustavo Torres <torresgustam@gmail.com>
2021-04-12 15:50:43 +0100

    add files

---
+++ project.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import math
+
+
+def main():
+    print("Hello World!")
+    
+if __name__ == "__main__":
+    main()
