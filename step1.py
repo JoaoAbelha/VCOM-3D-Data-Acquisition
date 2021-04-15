733bab98f0b6a95af1bb58e484321a3a4f05aa50
JoaoAbelha <jpabelha@hotmail.com>
2021-04-15 14:23:56 +0100

    added comments to step1

--- step1.py
+++ step1.py
@@ -3,9 +3,24 @@ import numpy as np
 import cv2 as cv
 import pickle
 
-############# intrinsic parameters ##################
-# this will enable us to correct the distortions
 
+'''
+    First step: Camera calibration, namely finding the intrinsic parameters of
+    the camera
+
+'''
+
+
+'''
+ Global configuration variables that are used in this first step
+ * IMAGES_PATH_EXPRESSION: where the chessboard images that are going to be used to calibrate da camera are
+ * PATTERN_SIZE: the pattern the algorithm is going to look for in the chessboard
+ * CHESSBOARD_SIZE: the amount of corners in the chessboard
+ * FIELD_SIZE: the real size of the boarders squares in mm, just to ease interpretation in later stages
+ * SHOW_IMAGES: if true, it shows the patterns found in the chessboards
+ * PATH_SAVE_INTRINSIC_PARAMS: where the intrinsic parameters are going to be saved
+ * SAVE_PARAMETERS: if true, the parameters are saved in a file
+'''
 IMAGES_PATH_EXPRESSION = './imgs/chessboard5/*.jpg'
 PATTERN_SIZE = (9, 6)
 CHECKBOARD_SIZE = (9, 6)
@@ -15,14 +30,28 @@ SHOW_IMAGES = False
 PATH_SAVE_INTRINSIC_PARAMS = "calibration/wide_dist_pickle.p"
 SAVE_PARAMETERS = True
 
-# if we are going to the always the same with the same camera we can save the values to use later
+'''
+* param {mtx}: camera matrix
+* param {dist}: distortion parameters
+* it saves the camera matrix and distortion parameters in a file
+* return void
+'''
 def save_intrinsicParameters(mtx, dist):
     to_save = {}
     to_save['mtx'] = mtx
     to_save['dist'] = dist
     pickle.dump(to_save,  open( PATH_SAVE_INTRINSIC_PARAMS, "wb" ))
     
-
+'''
+* param {objpoints} : relative object points
+* param {imgpoints}:  image points
+* param {mtx}: camera matrix
+* param {dist}: distortion parameters
+* param {rvecs}: rotation vectors
+* param {tvecs}: translation vectors
+* prints and calculates the reprojection error
+* return: void
+'''
 def reprojection_error(objpoints, imgpoints, mtx, dist, rvecs, tvecs):
     mean_error = 0
 
@@ -32,6 +61,13 @@ def reprojection_error(objpoints, imgpoints, mtx, dist, rvecs, tvecs):
         mean_error += error
     print("Reprojection error= {}".format(mean_error / len(objpoints)))
 
+
+'''
+* param {cameraMatrix} : the camera matrix
+* param {distCoeffs}: distortion coefficient of the camera 
+* prints the camera matrix and distortion coefficients of the camera
+* return: void
+'''
 def print_intrinsic_parameters(cameraMatrix, distCoeffs):
     print ("Camera Matrix = |fx  0 cx|")
     print ("                | 0 fy cy|")
@@ -42,6 +78,11 @@ def print_intrinsic_parameters(cameraMatrix, distCoeffs):
     print ("\nDistortion Coefficients = [k1, k2, p1, p2, k3]: {}".format(distCoeffs[0]))
 
 
+'''
+ * calibrate the camera by using a checkboard, it calculates the camera intrinsic parameters and distortion parameters
+   the parameters are saved in a file since we only need to calculate the parameters once for each camera 
+ * return void
+'''
 def camera_calibration():
     images = glob.glob(IMAGES_PATH_EXPRESSION)
     if len(images) == 0 :
@@ -62,12 +103,17 @@ def camera_calibration():
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+        # Find the chess board corners
         ret, corners = cv.findChessboardCorners(gray, CHECKBOARD_SIZE,\
                                                  cv.CALIB_CB_ADAPTIVE_THRESH+cv.CALIB_CB_NORMALIZE_IMAGE)
+
+        # If found, add object points, image points (after refining them)                                
         if ret:
             objPoints.append(object_points)
             corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1, -1), TERMINATION_CRITERIA)
             imgPoints.append(corners)
+
+            # Draw and display the corners if flag is true
             if SHOW_IMAGES:
                 cv.drawChessboardCorners(img, (9,6), corners2, ret)
                 cv.imshow('img', img)
